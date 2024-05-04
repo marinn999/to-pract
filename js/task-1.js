825d3b9a2f0bdb6c5e8ccd6c738aff231a8445c6
marinn999 <marinacompel@icloud.com>
2024-05-04 21:51:29 +0200

    module 2 added

--- js/task-1.js
+++ js/task-1.js
@@ -1047,3 +1047,107 @@
 // console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
 // console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
 // console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+// Станція з продажу ремонтних дроїдів готова до запуску, залишилося написати програмне забезпечення
+// для відділу продажів.Оголоси функцію makeTransaction(quantity, pricePerDroid, customerCredits),
+//     яка складає та повертає повідомлення про купівлю ремонтних дроїдів.
+// Вона оголошує три параметри, значення яких будуть задаватися під час її виклику:
+// quantity — кількість замовлених дроїдів
+// pricePerDroid — ціна одного дроїда
+// customerCredits — сума коштів на рахунку клієнта
+// function makeTransaction(quantity, pricePerDroid, customerCredits) {
+//   const sum = quantity * pricePerDroid;
+
+//   if (sum <= customerCredits) {
+//     return `You ordered ${quantity} droids worth ${sum} credits!`;
+//   } else {
+//     return 'Insufficient funds!';
+//   }
+// }
+// console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+// console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+// console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+// console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+// console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+
+//  Оголоси функцію formatMessage(message, maxLength), яка приймає рядок(параметр message) та перевіряє його
+//  довжину відповідно до заданої максимальної довжини(параметр maxLength).
+// Доповни код функції таким чином, що:
+// Якщо довжина рядка дорівнює або менша за maxLength, то функція повертає початковий рядок без змін.
+// Якщо довжина перевищує maxLength, то функція обрізає рядок до maxLength символів, додає трикрапку "..." в кінці
+// та повертає обрізану версію.
+
+// function formatMessage(message, maxLength) {
+//   const check = message.length;
+//   if (message.length > maxLength) {
+//     return `${message.slice(0, maxLength)}...`;
+//   } else {
+//     return message;
+//   }
+// }
+// console.log(formatMessage('Curabitur ligula sapien', 16)); // "Curabitur ligula..."
+// console.log(formatMessage('Curabitur ligula sapien', 23)); // "Curabitur ligula sapien"
+// console.log(formatMessage('Vestibulum facilisis purus nec', 20)); // "Vestibulum facilisis..."
+// console.log(formatMessage('Vestibulum facilisis purus nec', 30)); // "Vestibulum facilisis purus nec"
+// console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15)); // "Nunc sed turpis..."
+// console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41)); // "Nunc sed turpis a felis in nunc fringilla"
+
+// Функція checkForSpam(message) приймає рядок(параметр message), перевіряє його на вміст заборонених слів spam і sale,
+//     і повертає результат перевірки.Слова в рядку параметра message можуть бути в довільному регістрі, наприклад SPAM або sAlE.
+
+// Доповни код функції таким чином, що:
+// Якщо знайдено заборонене слово (spam або sale), то функція повертає буль true
+// Якщо в рядку відсутні заборонені слова, функція повертає буль false
+// function checkForSpam(message) {
+//   return (
+//     message.toLowerCase().includes('spam') ||
+//     message.toLowerCase().includes('sale')
+//   );
+// }
+// console.log(checkForSpam('Latest technology news')); // false
+// console.log(checkForSpam('JavaScript weekly newsletter')); // false
+// console.log(checkForSpam('Get best sale offers now!')); // true
+// console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+// console.log(checkForSpam('Trust me, this is not a spam message')); // true
+// console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+// console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+
+// Оголоси функцію getShippingCost(country), яка повинна перевіряти можливість доставки товару в країну
+// користувача(параметр country) і повертати повідомлення про результат.Обов'язково використовуй інструкцію switch.
+// Формат рядка, що повертається "Shipping to <country> will cost <price> credits", де замість < country > і < price >
+//     необхідно підставити відповідні значення.
+
+// Список країн і вартість доставки:
+// China — 100 кредитів
+// Chile — 250 кредитів
+// Australia — 170 кредитів
+// Jamaica — 120 кредитів
+
+// Зі списку видно, що доставка можлива не скрізь.Якщо зазначена країна відсутня у списку,
+//     то функція повинна повернути рядок "Sorry, there is no delivery to your country".
+// function getShippingCost(country) {
+//   let price = 0;
+//   switch (country) {
+//     case 'China':
+//       price = 100;
+//       break;
+//     case 'Chile':
+//       price = 250;
+//       break;
+//     case 'Australia':
+//       price = 170;
+//       break;
+//     case 'Jamaica':
+//       price = 120;
+//       break;
+//     default:
+//       return 'Sorry, there is no delivery to your country';
+//   }
+//   return `Shipping to ${country} will cost ${price} credits`;
+// }
+// console.log(getShippingCost('Australia')); // "Shipping to Australia will cost 170 credits"
+// console.log(getShippingCost('Germany')); // "Sorry, there is no delivery to your country"
+// console.log(getShippingCost('China')); // "Shipping to China will cost 100 credits"
+// console.log(getShippingCost('Chile')); // "Shipping to Chile will cost 250 credits"
+// console.log(getShippingCost('Jamaica')); // "Shipping to Jamaica will cost 120 credits"
+// console.log(getShippingCost('Sweden')); // "Sorry, there is no delivery to your country"
