ad44621b621d7bb54fadc55e22ec1576329bf782
marinn999 <marinacompel@icloud.com>
2024-05-17 15:38:59 +0200

    mod6

--- js/task-1.js
+++ js/task-1.js
@@ -2113,25 +2113,47 @@
 // child.age = 27;
 // console.log(child);
 
+// Створи клас Storage, який створюватиме об'єкти для управління складом товарів. Клас очікує лише один аргумент — початковий
+// масив товарів, який записується до створеного об'єкта в приватну властивість items.
+// Оголоси наступні методи класу:
+// getItems() — повертає масив поточних товарів у приватній властивості items.
+// addItem(newItem) — приймає новий товар newItem і додає його до масиву товарів у приватну властивість items об'єкта.
+// removeItem(itemToRemove) — приймає рядок з назвою товару itemToRemove і видаляє його з масиву товарів у приватній
+// властивості items об'єкта.
 
-Створи клас Storage, який створюватиме об'єкти для управління складом товарів. Клас очікує лише один аргумент — початковий 
-масив товарів, який записується до створеного об'єкта в приватну властивість items.
-Оголоси наступні методи класу:
-getItems() — повертає масив поточних товарів у приватній властивості items.
-addItem(newItem) — приймає новий товар newItem і додає його до масиву товарів у приватну властивість items об'єкта.
-removeItem(itemToRemove) — приймає рядок з назвою товару itemToRemove і видаляє його з масиву товарів у приватній 
-властивості items об'єкта.
+// class Storage {
+//     #items;
+//     construclor(elements) {
+//         this.#items = items;
+//     }
+//     getItems() {
+//         return this.#items;
+//     }
+//     addItem(newItem) {
 
-class Storage
+//     }
+// }
 
-const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
-storage.addItem("Droid");
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
 
-storage.removeItem("Prolonger");
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 
-storage.removeItem("Scaner");
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+// storage.removeItem("Scaner");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+// const startTime = Date.now();
+
+// // Твій код, виконуваний упродовж деякого часу
+// for (let i = 0; i <= 200; i += 1) {
+//   console.log(i);
+// }
+
+// const endTime = Date.now();
+// const elapsedTime = endTime - startTime;
+
+// console.log(`Elapsed time: ${elapsedTime} ms`);
